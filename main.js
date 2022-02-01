89caf085502a830f848b866c700893416b7ac926
Gauthamsudo <81172993+Gauthamsudo@users.noreply.github.com>
2022-02-01 13:10:09 +0530

    DiagnosticServices

---
+++ main.js
@@ -0,0 +1,56 @@
+// Burger menus
+document.addEventListener('DOMContentLoaded', function() {
+    // open
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
+
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    // close
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+});
+
+function sendMail(params) {
+    var tempParams = {
+      user_name: document.getElementById('username').value,
+      user_email: document.getElementById('useremail').value,
+      user_message: document.getElementById('usermsg').value, 
+    };
+  
+    emailjs.send('service_wce23qb', 'template_i1eo6p8',tempParams)
+    .then(function(res){
+      console.log("success", res.status);
+    })
+  
+    alert("Email Sent Successfully!")
+    return false;
+  }
